30fd9de66701f6fa2f5ec5acc9067215c71d9f90
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-07-22 08:47:54 +0300

    first task

--- git.sh
+++ git.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 # Get a random description word
-description_word=$(cat /usr/share/dict/words | shuf -n 2)
+if [ $# -ge 1 ];
+then
+        commit_word="$@"
+else
+        commit_word="upload"
+fi;
 
 # Add all the files in the current directory to the Git staging area
 git add .
 
 # Create a new commit with the random description word
-git commit -m "$description_word"
+git commit -m "$commit_word"
 
 # Push the changes to the remote repository
 git push
+
