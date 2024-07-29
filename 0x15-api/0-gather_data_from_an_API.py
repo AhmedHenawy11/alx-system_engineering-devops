6c9896b7a8f4d7c28814eef3bd5acfd382b195a2
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-07-29 19:16:36 +0300

    upload

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+print(f"argument id: {sys.argv[1]}")
+
+
+def main():
+    """ get data from api """
+
+    URL = "https://jsonplaceholder.typicode.com/"
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
