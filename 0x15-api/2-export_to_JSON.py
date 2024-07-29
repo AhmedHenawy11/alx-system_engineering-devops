40506c3f691e14d7f002d83e150171dd2a7ba446
AhmedHenawy11 <ahmedhenawy1511@gmail.com>
2024-07-29 23:20:49 +0300

    task 2 done

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -6,100 +6,34 @@ import sys
 
 
 def main():
-    """def com"""
-    id = sys.argv[1]
-    url = f"https://jsonplaceholder.typicode.com/"
-    users = f"users?id={id}"
-    todos = f"todos?userId={id}"
-    done = f"{todos}&completed=true"
-    notDone = f"{todos}&completed=false"
-    userData = requests.get(f"{url}{users}").json()
-    Name = userData[0].get("name")
-    userName = userData[0].get("username")
-    todosData = requests.get(f"{url}{todos}").json()
-    todosDone = requests.get(f"{url}{done}").json()
-    doneN = len(todosDone)
-    totalN = len(todosData)
-    """Export into json"""
-    with open(f"{id}.json", "w") as f:
+    """ main function to get data from api """
+
+    url = "https://jsonplaceholder.typicode.com/"
+    # User Info
+    user_id = sys.argv[1]
+    user_url = f"{url}users?id={user_id}"
+    user_data = requests.get(user_url).json()
+    user_name = user_data[0].get("name")
+    username = user_data[0].get("username")
+    # User Tasks
+    tasks_url = f"{url}todos?userId={user_id}"
+    total_tasks = requests.get(f"{tasks_url}").json()
+    done_tasks = requests.get(f"{tasks_url}&completed=true").json()
+    total_task_num = len(total_tasks)
+    done_tasks_num = len(done_tasks)
+
+    # Store in json file format
+    with (open(f'{user_id}.json', 'w') as file):
         data = {
-            id: [
-                {
-                    "task": task.get("title"),
-                    "completed": task.get("completed"),
-                    "username": userName,
-                }
-                for task in todosData
-            ]
+            user_id: [{
+                "task": task["title"],
+                "completed": task["completed"],
+                "username": username
+            }
+                for task in total_tasks]
         }
-        json.dump(data, f)
+        json.dump(data, file)
 
 
 if __name__ == "__main__":
     main()
-
-"""
-
-export json format
-{ "USER_ID": [{"task": "TASK_TITLE",
-"completed": TASK_COMPLETED_STATUS, "username": "USERNAME"},
- {"task": "TASK_TITLE", "completed":
-TASK_COMPLETED_STATUS, "username": "USERNAME"}, ... ]}
-File name must be: USER_ID.json
-
-
-export in CSV as : "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-USER_ID.csv
-
-
-
-
-Employee *NAME* is done with tasks(*DONE*/*TOTAL*):
-     *TITLE*
-     *TITLE*
-     *TITLE*
-
-
-https://jsonplaceholder.typicode.com/users?id=1
-{
-  "id": 1,
-  "name": "Leanne Graham",
-  "username": "Bret",
-  "email": "Sincere@april.biz",
-  "address": {
-    "street": "Kulas Light",
-    "suite": "Apt. 556",
-    "city": "Gwenborough",
-    "zipcode": "92998-3874",
-    "geo": {
-      "lat": "-37.3159",
-      "lng": "81.1496"
-    }
-  },
-  "phone": "1-770-736-8031 x56442",
-  "website": "hildegard.org",
-  "company": {
-    "name": "Romaguera-Crona",
-    "catchPhrase": "Multi-layered client-server neural-net",
-    "bs": "harness real-time e-markets"
-  }
-}
-https://jsonplaceholder.typicode.com/todos?userId=5
-[
-  {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
-  },
-  {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
-  },
-  ]
-
-https://jsonplaceholder.typicode.com/todos?userId=5&completed=true
-https://jsonplaceholder.typicode.com/todos?userId=5&completed=false
-  """
